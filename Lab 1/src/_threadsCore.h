6198833e8eccae66399a1bcfd46090ea0c63dff4
Rehanul Rehan Hakim <83771496+Rehanul-Hakim@users.noreply.github.com>
2022-10-24 17:11:07 -0400

    Changed comment
    One line comment to two line comment.

--- Lab 1/src/_threadsCore.h
+++ Lab 1/src/_threadsCore.h
@@ -6,7 +6,8 @@
 
 //Obtains the initial location of MSP by looking it up in the vector table
 uint32_t* getMSPInitialLocation(void); 
-//Returns the address of a new PSP with offset of "offset" bytes from MSP. Be careful with pointer arithmetic! It's best to cast to an integer then back if you're not sure
+//Returns the address of a new PSP with offset of "offset" bytes from MSP. 
+//Be careful with pointer arithmetic! It's best to cast to an integer then back if you're not sure
 uint32_t* getNewThreadStack(uint32_t offset); 
 //Sets up the threads
 int createThread (void (*task)(void* args));
