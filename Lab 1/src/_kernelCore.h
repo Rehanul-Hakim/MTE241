85a3af02d6af86984319bd029c072846eaba65bc
Joyce <77315550+joycechen9@users.noreply.github.com>
2022-10-25 20:03:03 -0400

    Got rid of unnecessary osSched declaration

--- Lab 1/src/_kernelCore.h
+++ Lab 1/src/_kernelCore.h
@@ -4,9 +4,6 @@
 //initializes memory structures and interrupts necessary to run the kernel
 void kernelInit(void);
 
-//called by the kernel to schedule which threads to run
-void osSched(void);	
-
 //initialize anything that the first thread needs before it gets going, and switches between PSP and MSP
 void kernel_start(void);	
 
